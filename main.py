26ac2e591e58d566af643601c3be394a3f74aa24
Jomi <134692110+Nineee1@users.noreply.github.com>
2025-10-08 17:02:44 -0400

    Auto assign ticket to least busy member

--- main.py
+++ main.py
@@ -9,46 +9,46 @@ url = "https://wusujomi1.atlassian.net"
 jira_connect = JIRA(server = url, basic_auth = (email, token))
 print ("Connected")
 
-#Project Key
+#Project Key/Members
 proj_key = "SUP"
-
-"""
-testIssues = [
-    {"summary": "Can't log in", "description": "User cannot log in to email", "issuetype": "Submit a request or incident", "priority": "High"},
-    {"summary": "Printer not working", "description": "Office printer jammed", "issuetype": "Submit a request or incident", "priority": "High"},
-    {"summary": "Software install request", "description": "Install Microsoft Teams", "issuetype": "Submit a request or incident", "priority": "High"},
-]
-
-
-for t in testIssues:
-    issue = jira_connect.create_issue(
-        project=proj_key,
-        summary=t["summary"],
-        description=t["description"],
-        issuetype={"name": t["issuetype"]},
-        priority={"name": t["priority"]}
-    )
-    print(f"Created ticket {issue.key}")
-"""
-
 members = []
 
-users = jira_connect.search_users(query=' ',maxResults = 50) #Gather users in project
+#Gather users in project
+users = jira_connect.search_users(query=' ',maxResults = 50) 
 
 for u in users:
     name = getattr(u, "displayName").lower().strip()
-    if not any(word in name for word in ["automation", "jira", "atlassian", "alert", "trello", "slack", "Proforma"]): #Check if user name consists of these strings
-        #Add members to array
-        members.append(f"name: {u.displayName}") 
-        members.append(f"id: {u.accountId}")
+    #Check if user name consists of these strings
+    if not any(word in name for word in ["automation", "jira", "atlassian", "alert", "trello", "slack", "proforma"]):
+        #Add member to array
+        members.append({f"name": u.displayName, "id": u.accountId})
     
+print(f'Members = {len(members)}')
 
-    get_least_busy(jira_connect, members)
-
-def get_least_busy(jira, agents):
+def get_least_busy(jira, members):
     workload = {} #Array of work
-    for agent in agents:
-        issues = jira.search_issues(f'assignee={agent["id"]} AND status != Done') #Search tickets for agent that hasn't been done
-        workload[agent["id"]] = len(issues) #Assign issues amount to angent
-    available_angent = min(workload) # least busy agent is the one with the least issues
-    return available_angent 
+    for member in members:
+        #Search tickets for agent that hasn't been done using a jql query
+        issues = jira.search_issues(f'assignee = "{member["id"]}" AND status != Done')
+        #Assign issues amount to angent
+        workload[member["id"]] = len(issues) 
+
+    # least busy agent is the one with the least issues
+    available_member = min(workload, key=workload.get) 
+    #Returns the dictionairy of the member that is the least busy/available
+    return next(member for member in members if member["id"] == available_member)
+
+def search_unassigned_issue(key, jira):
+    #Returns all tickets
+    return jira.search_issues(f'project={key} AND assignee is EMPTY AND status != Done')
+
+tickets = search_unassigned_issue(proj_key, jira_connect)
+print(f'Ticket Amount = {len(tickets)}')
+
+if tickets == 0:
+     print("No Tickets")
+else:
+     for issue in search_unassigned_issue(proj_key, jira_connect):
+        member = get_least_busy(jira_connect,members)
+        issue.update(fields={"assignee": {"id": member["id"]}})
+        print(f'Assigned {issue.key} to {member["name"]}')
