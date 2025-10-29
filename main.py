2a13444a59066b3404334aebaa54b03625e8ec82
Jomi <134692110+Nineee1@users.noreply.github.com>
2025-10-29 19:15:08 -0400

    Scheduler

--- main.py
+++ main.py
@@ -43,67 +43,8 @@ except Exception as e:
 proj_key = "SUP"
 members = []
 
+
 '''
-def get_least_busy(jira, members):
-    workload = {} #Array of work
-    for member in members:
-        try:
-            #Search tickets for agent that hasn't been done using a jql query
-            ticket = jira.search_issues(f'assignee = "{member["id"]}" AND status != closed AND status != resolved')
-            #Assign issues amount to angent
-            workload[member["id"]] = len(ticket) 
-
-        except Exception as e:
-            logging.error(f"Error getting workload for {member["name"]} {e}")
-
-    # least busy agent is the one with the least issues
-    available_member = min(workload, key=workload.get) 
-    #Returns the dictionairy of the member that is the least busy/available
-    return next(member for member in members if member["id"] == available_member)
-
-def search_unassigned_issue(key, jira):
-    try:
-        #Returns all tickets that has no assigned member and not done
-        return jira.search_issues(f'project={key} AND assignee is EMPTY AND status != closed AND status !=resolved')
-    except Exception as e:
-        logging.error(f"Error gathering unassigned issues: {e}")
-
-def close_resolved_tickets_auto(jira):
-    try:
-        #Jquery to check if the ticket is resolved or not
-        jquery = f'project={proj_key} AND status ="Resolved"'
-        resolved_ticket = jira.search_issues(jquery)
-
-        for ticket in resolved_ticket:
-            #Checks if ticket has a resolved status
-            if ticket.fields.status.name.lower() == "resolved":
-                #Gets the time of the resolved date and using the datetime strptime method it converts into a specific format
-                resolved_date = datetime.strptime(ticket.fields.statuscategorychangedate[:10], "%Y-%m-%d")
-        
-            #if the date of the resolved date has exceeded 2 day it will close
-            if datetime.now() - resolved_date > timedelta(days=2):
-                
-                #Gets all transitions in the ticket EX Start Progress - In progress etc
-                transitions = jira.transitions(ticket)
-                close_ticket = next((t for t in transitions if t['name'].lower() == 'close'))
-                
-                #Checks if there is a resolved transition and closes ticket/sends email
-                if close_ticket:
-                    user = jira_connect.user(f"{ticket.fields.assignee.accountId}")
-                    reciever_email = next((member["email"] for member in members if user.accountId == member["id"]), None)
-                    subject = f"Ticket {ticket.key} has been automatically closed."
-
-                    body = (
-                        f"{ticket.fields.summary} has been closed after being left on resolved for more than 2 days"
-                        f"{jira_connect.server_url}/browse/{ticket.key}"
-                    )
-
-                    send_email(reciever_email, subject, body)
-                    #Moves ticket from one status to done status and closes ticket
-                    jira.transition_issue(ticket, close_ticket['id'])
-                    print(f"Ticket Closed ({ticket.key})")
-    except Exception as e:
-        logging.error(f"Error closing resolved tickets: {e}")
 
 def remind_old_tickets(jira):
     try:
@@ -132,22 +73,6 @@ def remind_old_tickets(jira):
 
     except Exception as e:
         logging.error(f"Error sending old ticket reminders: {e}")
-
-def send_email(reciever, subject, body):
-    #Email formatting using MIMEText for readability in the email 
-    msg = MIMEText(body)
-    msg["Subject"] = subject
-    msg["From"] = email_sender
-    msg["To"] = reciever
-
-    try:
-        #Connects to gmail login to email and sends message close connection
-        with smtplib.SMTP(email_server, email_port) as server:
-            server.starttls()
-            server.login(email_sender, email_pass)
-            server.send_message(msg)
-    except Exception as e:
-        logging.error(f"Error sending email {e}")
         
 def send_weekly_analytics(jira, proj_key):
     
@@ -216,22 +141,6 @@ def create_test_tickets(jira, proj_key):
 time.sleep(5)
 
 try:
-    ticket_amount = jira_connect.search_issues(f'project={proj_key}')
-    tickets_unassigned_amount = search_unassigned_issue(proj_key, jira_connect)
-    logging.info(f"Ticket Amount: {len(ticket_amount)}, Unassigned Ticket Amount: {len(tickets_unassigned_amount)}")
-
-    #Check if no tickets available if there is assign least busy worker
-    if len(tickets_unassigned_amount) == 0:
-        logging.info("No unassinged tickets found.")
-    else:        
-        for ticket in tickets_unassigned_amount:
-            try:
-                member = get_least_busy(jira_connect,members)
-                #Update the ticket to assign the least busy member to it
-                ticket.update(fields={"assignee": {"id": member["id"]}})
-                logging.info(f'Assigned {ticket.key} to {member["name"]}')
-            except Exception as e:
-                logging.error(f"Failed to assign ticket {ticket.key} to {member["name"]}")
 
     #close_resolved_tickets_auto(jira_connect)
     #remind_old_tickets(jira_connect)
@@ -242,7 +151,7 @@ try:
 except Exception as e:
     logging.critical(f"Main program error: {e}")
 
-'''    
+   '''
 class jiraDash:
     def __init__(self, root):
         self.root = root
@@ -254,9 +163,31 @@ class jiraDash:
         self.jira_connect = None
         self.members = []
 
+        self.total_tickets = 0
+        self.unassigned_tickets = 0
+        self.assigned_tickets = 0
+        self.closed_tickets = 0
+
         self.ui_create()
         self.logging()
         self.connect_jira()
+        self.ticket_monitoring()
+        self.start_schedule()
+        self.close_resolved_tickets_auto()
+
+    def start_schedule(self):
+        #every 60s
+        schedule.every(60).seconds.do(self.ticket_monitoring)
+        schedule.every(1).hour.do(lambda:self.run_in_thread(self.close_resolved_tickets_auto))
+
+        #Run in thread so gui is responsive
+        threading.Thread(target=self.schedule_loop, daemon=True).start()
+
+    def schedule_loop(self):
+        #Look and run for any pending schedules
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
     def ui_create(self):
         #Create widget that contains
@@ -300,6 +231,9 @@ class jiraDash:
         #Create buttons using custom style
         self.close_btn = CTkButton(self.left, text="Ticket Overview",  command=lambda: self.run_in_thread(self.gather_tickets), **css_style)
         self.close_btn.pack(pady=10, padx=10)
+
+        self.close_btn = CTkButton(self.left, text="Set resolved tickets to closed",  command=lambda: self.run_in_thread(self.close_resolved_tickets_auto), **css_style)
+        self.close_btn.pack(pady=10, padx=10)
     
     def logging(self):
         class handler(logging.Handler):
@@ -312,6 +246,8 @@ class jiraDash:
                 #Configure logging messages
                 msg = self.format(record)
                 #Temporarily unlocks text box and add to it
+                self.text_widget.after(0, self.append_msg, msg)
+            def append_msg(self, msg):
                 self.text_widget.configure(state="normal")
                 self.text_widget.insert("end", msg + "\n")
                 self.text_widget.see("end")
@@ -347,31 +283,120 @@ class jiraDash:
             logging.error(f"Failed to connect or get members: {e}")
             messagebox.showerror("Couldn't connect to jira")
 
+    def get_least_busy(self):
+        workload = {} #Array of work
+        for member in self.members:
+            try:
+                #Search tickets for agent that hasn't been done using a jql query
+                ticket = self.jira_connect.search_issues(f'assignee = "{member["id"]}" AND status != closed AND status != resolved')
+                #Assign issues amount to angent
+                workload[member["id"]] = len(ticket) 
+
+            except Exception as e:
+                logging.error(f"Error getting workload for {member["name"]} {e}")
+
+        # least busy agent is the one with the least issues
+        available_member = min(workload, key=workload.get) 
+        #Returns the dictionairy of the member that is the least busy/available
+        return next(member for member in self.members if member["id"] == available_member)
+    
+    def ticket_monitoring(self):
+        logging.info("Started monitoring for new tickets")
+        self.gather_tickets()
+
+        #Check if no tickets available if there is assign least busy worker
+        if len(self.unassigned_tickets) == 0:
+            logging.info("No unassinged tickets found.")
+            return
+        else:
+            for ticket in self.unassigned_tickets:
+                try:
+                    member = self.get_least_busy()
+                    #Update the ticket to assign the least busy member to it
+                    ticket.update(fields={"assignee": {"id": member["id"]}})
+                    logging.info(f'Assigned {ticket.key} to {member["name"]}')
+                except Exception as e:
+                    logging.error(f"Failed to assign ticket {ticket.key} to {member["name"]}")
+    
+
     def gather_tickets(self):
         try:
             #Returns all tickets (Unassigned, assigned, closed)
-            all_tickets = self.jira_connect.search_issues(f'project={proj_key}')
-            unassigned_tickets = self.jira_connect.search_issues(f'project={proj_key} AND assignee is EMPTY AND status != closed AND status !=resolved')
-            assigned_tickets = self.jira_connect.search_issues(f'project={proj_key} AND assignee is not EMPTY')
-            closed_tickets = self.jira_connect.search_issues(f'project={proj_key} AND status = close')
-            logging.info(f"Total Tickets - {len(all_tickets)}")
-            logging.info(f"Unassigned Tickets - {len(unassigned_tickets)}")
-            logging.info(f"Assigned Tickets - {len(assigned_tickets)}")
-            logging.info(f"Closed Tickets - {len(closed_tickets)}")
+            self.total_tickets = self.jira_connect.search_issues(f'project={proj_key}')
+            self.unassigned_tickets = self.jira_connect.search_issues(f'project={proj_key} AND assignee is EMPTY AND status != closed AND status !=resolved')
+            self.assigned_tickets = self.jira_connect.search_issues(f'project={proj_key} AND assignee is not EMPTY')
+            self.closed_tickets = self.jira_connect.search_issues(f'project={proj_key} AND status = close')
+            logging.info(f"Total Tickets - {len(self.total_tickets)}")
+            logging.info(f"Unassigned Tickets - {len(self.unassigned_tickets)}")
+            logging.info(f"Assigned Tickets - {len(self.assigned_tickets)}")
+            logging.info(f"Closed Tickets - {len(self.closed_tickets)}")
             return members
         except Exception as e:
             logging.error(f"Error gathering tickets {e}")
 
+    def send_email(self,reciever, subject, body):
+        #Email formatting using MIMEText for readability in the email 
+        msg = MIMEText(body)
+        msg["Subject"] = subject
+        msg["From"] = email_sender
+        msg["To"] = reciever
 
+        try:
+            #Connects to gmail login to email and sends message close connection
+            with smtplib.SMTP(email_server, email_port) as server:
+                server.starttls()
+                server.login(email_sender, email_pass)
+                server.send_message(msg)
+        except Exception as e:
+            logging.error(f"Error sending email {e}")
+    
+    def close_resolved_tickets_auto(self):
+        try:
+            #Jquery to check if the ticket is resolved or not
+            jquery = f'project={proj_key} AND status ="Resolved"'
+            resolved_ticket = self.jira_connect.search_issues(jquery)
+
+            if len(resolved_ticket) > 0:
+                for ticket in resolved_ticket:
+
+                    #Checks if ticket has a resolved status
+                    #Gets the time of the resolved date and using the datetime strptime method it converts into a specific format
+                    resolved_date = datetime.strptime(ticket.fields.statuscategorychangedate[:10], "%Y-%m-%d")
+                
+                    #if the date of the resolved date has exceeded 2 day it will close
+                    if datetime.now() - resolved_date > timedelta(days=2):
+                        
+                        #Gets all transitions in the ticket EX Start Progress - In progress etc
+                        transitions = self.jira_connect.transitions(ticket)
+                        close_ticket = next((t for t in transitions if t['name'].lower() == 'close'))
+                        
+                        #Checks if there is a resolved transition and closes ticket/sends email
+                        if close_ticket:
+                            user = self.jira_connect.user(f"{ticket.fields.assignee.accountId}")
+                            reciever_email = next((member["email"] for member in self.members if user.accountId == member["id"]), None)
+                            subject = f"Ticket {ticket.key} has been automatically closed."
+
+                            body = (
+                                f"{ticket.fields.summary} has been closed after being left on resolved for more than 2 days"
+                                f"{self.jira_connect.server_url}/browse/{ticket.key}"
+                            )
+
+                            self.send_email(reciever_email, subject, body)
+                            #Moves ticket from one status to done status and closes ticket
+                            self.jira_connect.transition_issue(ticket, close_ticket['id'])
+                            logging.info(f"Sent Email (Ticket {ticket.key} left resolved for more than 2 days)")
+            else:
+                logging.info("No resolved tickets left for more than 2 days")
+        except Exception as e:
+            logging.error(f"Error closing resolved tickets: {e}")
+                
+    
     def run_in_thread(self, func):
         threading.Thread(target=func).start()
-
+    
 
     def close_tickets(self):
         print("Closed")
-
-
-
         
 if __name__ == "__main__":
     root = CTk()
